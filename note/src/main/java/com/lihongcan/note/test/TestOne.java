4cdbeecc82725dd2fe284fb1fe78195345e354c5
lihongcan <598753743@qq.com>
2016-08-04 15:47:23 +0800

    add some test

--- note/src/main/java/com/lihongcan/note/test/TestOne.java
+++ note/src/main/java/com/lihongcan/note/test/TestOne.java
@@ -4,4 +4,19 @@ package com.lihongcan.note.test;
  * Created by lihongcan on 2016/7/15.
  */
 public class TestOne {
+    private static TestOne instance;
+    private TestOne(){
+
+    }
+    public static TestOne getInstance(){
+        if (null==instance)
+        {
+            instance=new TestOne();
+        }
+        return instance;
+    }
+    public static void  main(String[] args){
+        TestOne testOne=TestOne.getInstance();
+        System.out.println(testOne);
+    }
 }
